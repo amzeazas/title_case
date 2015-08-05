07de038f6ebda7aa1f1c2531175f53dc817ca5a9
Rachel Berecka & Alexa Zeazas <amzeazas@comcast.net>
2015-08-05 09:37:43 -0700

    Change example from green eggs and ham to lord of the rings.

--- spec/title_case_integration_spec.rb
+++ spec/title_case_integration_spec.rb
@@ -6,8 +6,8 @@ set(:show_exceptions, false)
 describe('the title case path', {:type => :feature}) do
   it('processes the user entry and returns it title cased') do
     visit('/')
-    fill_in('title', :with => 'green eggs and ham')
+    fill_in('title', :with => 'lord of the rings')
     click_button('Send')
-    expect(page).to have_content('Green Eggs and Ham')
+    expect(page).to have_content('Lord of the Rings')
   end
 end
